5c549855e590d483d67252d1f921008c6f07a8f1
lokesh.cardinalts <lokesh.tulsani@cardinalts.com>
2014-03-10 12:29:32 +0530

    Test cordova.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 Zepto(function($){
-	$('#test').on('click', function() {
-		myApp.setElementHTML($('#myContent'), Date());
-	});
+	myApp.init();
+	
 });
--- js/my_app.js
+++ js/my_app.js
@@ -1,9 +1,39 @@
 var myApp = (function(module) {
 	
-	module.setElementHTML = function(elementObject, newContent){
-		elementObject.html(newContent);
-	}
+	// Wait for device API libraries to load	//
+	module.init = function() {
+		document.addEventListener("deviceready", this.onDeviceReady, false);
+		document.addEventListener("pause", this.onPause, false);
+		document.addEventListener("resume", this.onResume, false);
+		document.addEventListener("backbutton", this.onBackKeyDown, false);
+		document.addEventListener("menubutton", this.onMenuKeyDown, false);
+	};
 	
+	// device APIs are available. Now safe to use device APIs
+	module.onDeviceReady = function () {
+		alert('API Ready');
+	};
+
+	// Handle the pause event
+	module.onPause = function() {
+	};
 	
+	// Handle the pause event
+	module.onResume = function() {
+		setTimeout(function() {
+			alert('Welcome Back');
+		}, 0);
+	};
+
+	// Handle the back button
+	module.onBackKeyDown = function() {
+			alert('Goin Back');
+	};
+
+	// Handle the back button
+	module.onMenuKeyDown = function() {
+			alert('Goin Menu');
+	};
+
 	return module;
 }(myApp || {}));
